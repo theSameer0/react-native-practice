9b5842641cf47e17a2acabf28f65f28e15141465
Sameer Mishra <sameermishra@IM-HYD-LAP-0241.local>
2023-03-03 12:31:54 +0530

    updated android file

--- src/api/modal/fetchData.tsx
+++ src/api/modal/fetchData.tsx
@@ -1,13 +1,38 @@
-const GET_MOVIE_URL = 'http://localhost:8080/movies'
-export default async function getMovies () {
-  fetch(GET_MOVIE_URL)
-  .then(
-    response => response.json()
-  )
-  .then(
-    data => console.log(data)
-  )
-  .catch((err)=>{
-    console.log(err)
-  })
-};
+import axios from "axios"
+import { useEffect } from "react";
+
+const GET_MOVIE_URL = 'http://localhost:9090'
+const config = {
+  headers: {
+    "Access-Control-Allow-Origin":"*",
+    "Access-Control-Allow-Credentials": true,
+  }
+}
+
+export default function getMovies() {
+  const fetchData = () => {
+    const baseURL = "https://api.sampleapis.com/coffee";
+    axios.get(`${GET_MOVIE_URL}/movies`, config)
+    .then(
+      (response) => console.log(response.data)
+    )
+    .catch((err)=>console.log(err))
+  };
+  fetchData()
+}
+
+
+
+
+// export default async function getMovies () {
+//   fetch(GET_MOVIE_URL)
+//   .then(
+//     response => response.json()
+//   )
+//   .then(
+//     data => console.log(data)
+//   )
+//   .catch((err)=>{
+//     console.log(err)
+//   })
+// };
